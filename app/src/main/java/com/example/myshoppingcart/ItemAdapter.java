2a568148039c73420dd4a8165f7aeb5ca84ab297
Fatah <bnb.fatah@gmail.com>
2022-12-31 11:37:21 +0100

    fix hide item - ignored badge

--- app/src/main/java/com/example/myshoppingcart/ItemAdapter.java
+++ app/src/main/java/com/example/myshoppingcart/ItemAdapter.java
@@ -9,7 +9,6 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.snackbar.Snackbar;
